d4b150f5a8363dda4de6b0a0d014bc12cb49f23a
eduar2tc@gmail.com <eduar2tc@gmail.com>
2022-01-27 04:10:30 -0600

    button clear and col history added

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let result = document.querySelector('#result');
     let buttonEncryptListener = document.querySelector('.container-buttons button:nth-child(1)');
     let buttonDecryptListener = document.querySelector('.container-buttons button:nth-child(2)');
-    let buttonCopyTextListener = document.querySelector('.container-boxes .col:nth-child(2) button');
+    let buttonCopyTextListener = document.querySelector('.container-boxes .col:nth-child(2) .container-buttons button:nth-child(2)');
+    let buttonClearAll = document.querySelector('.container-boxes .col:nth-child(2) .container-buttons button:nth-child(1)');
+    let listResults = document.querySelector('.list-results ol');
     let mapToEncrypt = {
         a: 'ai',
         e: 'enter',
@@ -58,6 +60,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     buttonCopyTextListener.addEventListener('click', function(){
         let text = result.value;
+        let textSpan = document.querySelector('.col:nth-child(2) button:nth-child(2) span');
         if( text.length > 0 ){
             navigator.clipboard.writeText(text).then(
                 /* clipboard successfully set */
@@ -66,10 +69,10 @@ document.addEventListener("DOMContentLoaded", function () {
                     textInput.focus();
                     buttonCopyTextListener.style.transition = 'width 0.3s ease-out';
                     buttonCopyTextListener.style.width = '7rem';
-                    buttonCopyTextListener.innerHTML = 'Copied!';
+                    textSpan.innerHTML = 'Copied!';
                     setTimeout(() => { 
                         buttonCopyTextListener.style.width = '5rem';
-                        buttonCopyTextListener.innerHTML = 'Copy'; 
+                        textSpan.innerHTML = 'Copy';
                     }, 1000);
                 },
                 /* clipboard write failed */
@@ -83,5 +86,29 @@ document.addEventListener("DOMContentLoaded", function () {
             textInput.placeholder = 'Pls insert a text';
         }
     } );
+    buttonClearAll.addEventListener('click', function(){
+        let textSpan = document.querySelector('.col:nth-child(2) button:nth-child(1) span');
+        if (textInput.value.length > 0 && result.value.length || textInput.value.length > 0 || result.value.length > 0 ){
+            //listResults.style.transition = '0.3s ease-out';
+            //listResults.style.opacity = 0;
+            listResults.innerHTML += '<li class="item-list">'+result.value+'</li>';
+            textInput.value = '';
+            result.value = '';
+            buttonClearAll.style.transition = 'width 0.3s ease-out';
+            buttonClearAll.style.width = '7.5rem';
+            textSpan.innerHTML = 'Cleared!';
+            setTimeout(() => {
+                let itemList = document.querySelector('.col:nth-child(3) li:last-child');
+                itemList.classList.add('item-list');
+                itemList.style.opacity = 1;
+                buttonClearAll.style.width = '5.5rem';
+                textSpan.innerHTML = 'Clear';
+            }, 1000);
+        }else{
+            textInput.focus();
+            textInput.style.outlineColor = 'indianred';
+            textInput.placeholder = 'No elements to clear';
+        }
+    });
 
 });
